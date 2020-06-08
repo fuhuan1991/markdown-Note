d555d1d78b4058dbc78b0e6ee50daffffcbbc1ac
fuhuan1991 <huanfu2019@gmail.com>
2020-06-08 11:29:17 -0400

    change structure

---
+++ README.md
@@ -0,0 +1,22 @@
+
+## Intro
+This is markdown editor. For now, this is only a front-end part of a online markdown note app.
+- Type markdown script on the left, the compiled result would appear on the right.
+- To insert some specific markdown pattern, use the buttons on the top.
+- The side bar menu is just a decoration for now.
+
+
+## Tech Stack
+- React
+- Antd
+- codemirror
+- markdown parser
+
+
+## Todo list:
+1. back-end storage system
+2. folder system
+3. user system
+
+## Try it
+https://fuhuan1991.github.io/markdown-Note/index.html
---
+++ docs/precache-manifest.7bb8dcb0fad035bdd05e02680fc635bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e123a56c6e7dc64ac05c439b9efdd115",
+    "url": "/markdown-Note/index.html"
+  },
+  {
+    "revision": "f6412c5bdc63be10264b",
+    "url": "/markdown-Note/static/css/2.6f2dcc96.chunk.css"
+  },
+  {
+    "revision": "dbfa684a508964432428",
+    "url": "/markdown-Note/static/css/main.a082d3b0.chunk.css"
+  },
+  {
+    "revision": "f6412c5bdc63be10264b",
+    "url": "/markdown-Note/static/js/2.ebdcce7b.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/markdown-Note/static/js/2.ebdcce7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbfa684a508964432428",
+    "url": "/markdown-Note/static/js/main.274e2f23.chunk.js"
+  },
+  {
+    "revision": "4a5488beba4f35494950",
+    "url": "/markdown-Note/static/js/runtime-main.3b2bad10.js"
+  }
+]);
