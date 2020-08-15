5944d9c61d31248bf598109987873c83865f3422
fuhuan1991 <huanfu2019@gmail.com>
2020-08-15 12:39:59 -0400

    modify note module

--- front_end/src/mkNote/index.jsx
+++ front_end/src/mkNote/index.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import MkEditor from './mkEditor/index';
 import MkDisplay from './mkDisplay/index';
+import PropTypes from 'prop-types';
+import { getContent } from '../api/client';
+import { Spin, Slider } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import { updateNote } from '../api/client';
+import { withRouter } from "react-router";
+import { notify } from '../notification';
+
 import './style.scss';
+
+const Loading = <LoadingOutlined style={{ fontSize: 120 }} spin />;
 
 class MkNote extends React.Component {
 
@@ -9,24 +19,138 @@ class MkNote extends React.Component {
     super(props);
     this.state = {
       source: '',
+      ready: false,
+      changed: false,
+      sizeConfig: 1,
     }
   }
 
+  componentDidMount() {
+    const { noteId } = this.props;
+    // console.info("noteId", noteId)
+    this.getNewContent(noteId);
+  }
+
+  componentDidUpdate(prevProps) {
+    const { noteId } = this.props;
+    const { noteId: oldId } = prevProps;
+
+    if (oldId !== noteId) {
+      this.getNewContent(noteId);
+    }
+
+  }
+
+  getNewContent = (noteId) => {
+    this.setState({
+      ready: false,
+    });
+    getContent(noteId).then(
+      (res) => {
+        this.setState({
+          source: res.text,
+          ready: true,
+          changed: false,
+        });
+      },
+      () => {}
+    );
+  }
+
   onEditorChange = (code) => {
+    if (this.state.source === code) return;
     this.setState({
       source: code,
+      changed: true,
     });
   }
 
+  shouldComponentUpdate(nextProps) {
+
+    // auto saving
+    if (nextProps.location.pathname !== this.props.location.pathname) {
+
+      const doc = window.codeMirror.getDoc();
+      const noteId = this.props.location.pathname.slice(6);
+      const changed = this.state.changed;
+  
+      if (!doc || !noteId || noteId.length < 1 || !changed) return true;
+  
+      const text = doc.getValue();
+      updateNote(noteId, text).then(
+        (res) => {
+          notify('success', "Auto saving completed \n" + res);
+        },
+        () => {
+          notify('error', 'update failed, try again later');
+        }
+      );
+    }
+    return true;
+  }
+
+  componentWillUnmount() {
+    // auto saving
+    const doc = window.codeMirror.getDoc();
+    const noteId = this.props.location.pathname.slice(6);
+    const changed = this.state.changed;
+
+    if (!doc || !noteId || noteId.length < 1 || !changed) return true;
+
+    const text = doc.getValue();
+    updateNote(noteId, text).then(
+      (res) => {
+        notify('success', "Auto saving completed \n" + res);
+      },
+      () => {
+        notify('error', 'update failed, try again later');
+      }
+    );
+  }
+
+  sizeChange = (v) => {
+    this.setState({sizeConfig: v});
+  }
+
   render() {
-    const className = this.props.isSidebarDeployed ? 'main mk-mode side-bar-deployed' : 'main mk-mode';
+
+    const { source, ready, sizeConfig } = this.state;
+
     return (
-      <div className={className}>
-        <MkEditor onChange={this.onEditorChange} />
-        <MkDisplay source={this.state.source} />
+      <div className="main mk-mode">
+        {!ready && <Spin indicator={Loading} />}
+        {!!ready && 
+          <>
+            <MkEditor 
+              onChange={this.onEditorChange}
+              initialValue={source} 
+              shrink={sizeConfig === 0}
+              expand={sizeConfig === 2}
+            />
+            <MkDisplay 
+              source={this.state.source} 
+              shrink={sizeConfig === 2}
+              expand={sizeConfig === 0}
+            />
+            <div className="slider-bar">
+              <Slider 
+                max={2} 
+                defaultValue={1} 
+                included={false}
+                tooltipVisible={false} 
+                onChange={this.sizeChange}
+                value={sizeConfig}
+              />
+            </div>
+          </>
+        }
       </div>
     );
   }
 }
 
-export default MkNote;
+MkNote.propTypes = {
+  noteId: PropTypes.string,
+};
+
+export default withRouter(MkNote);
--- front_end/src/mkNote/mkDisplay/index.jsx
+++ front_end/src/mkNote/mkDisplay/index.jsx
@@ -3,8 +3,15 @@ import Markdown from 'react-markdown';
 import './style.scss';
 
 const MkDisplay = (props) => {
+
+  const { shrink, expand } = props;
+  let className = 'mk-display';
+
+  if (shrink) className += ' shrink';
+  if (expand) className += ' expand';
+
   return (
-    <div className='mk-display'>
+    <div className={className}>
       <Markdown source={props.source} escapeHtml={false}/>
     </div>
   );
