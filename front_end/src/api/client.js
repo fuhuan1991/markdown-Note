d5231c3285464ef39759724e63dd3c33dc017a77
fuhuan1991 <huanfu2019@gmail.com>
2020-08-16 11:24:27 -0400

    added offline APIs

--- front_end/src/api/client.js
+++ front_end/src/api/client.js
@@ -1,107 +1,51 @@
-import fetch from 'unfetch';
-import constructMenu from './constructMenuJSON';
-import { notify } from '../notification';
+import serverAPI from './serverStorage';
+import offlineAPI from './offlineStorage';
 
-const baseUrl = 'http://localhost:8080/';
 
-// check the status of all sort of response
-const checkStatus = response => {
-  // console.log("response", response);
-  if (response.ok) {
-    return response;
-  } else {
-    response.json().then(o => {
-      notify('error', o.msg);
-    });
-    return Promise.reject();
-  }
+const offline = true;
+
+let getMenu;
+let getContent;
+let createNote;
+let createDir;
+let deleteDir;
+let deleteNote;
+let renameDir;
+let renameNote;
+let updateNote;
+
+if (offline) {
+  getMenu = offlineAPI.getMenu;
+  getContent = offlineAPI.getContent;
+  createNote = offlineAPI.createNote;
+  createDir = offlineAPI.createDir;
+  deleteDir = offlineAPI.deleteDir;
+  deleteNote = offlineAPI.deleteNote;
+  renameDir = offlineAPI.renameDir;
+  renameNote = offlineAPI.renameNote;
+  updateNote = offlineAPI.updateNote;
+} else {
+  getMenu = serverAPI.getMenu;
+  getContent = serverAPI.getContent;
+  createNote = serverAPI.createNote;
+  createDir = serverAPI.createDir;
+  deleteDir = serverAPI.deleteDir;
+  deleteNote = serverAPI.deleteNote;
+  renameDir = serverAPI.renameDir;
+  renameNote = serverAPI.renameNote;
+  updateNote = serverAPI.updateNote;
 }
 
-// get menu list from back-end and then construct the menu structure
-export const getMenu = data => fetch(baseUrl + 'api/dir/getMenu/', {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  method: 'GET',
-  body: JSON.stringify(data),
-})
-.then(checkStatus)
-.then(response => {
-  return response.json();
-}, e => {
-  return e;
-})
-.then(arr => {
-  if (Array.isArray(arr)) {
-    return constructMenu(arr);
-  } else {
-    return Promise.reject();
-  }
-});
 
-// create a new notebook(directory)
-export const createDir = data => fetch(baseUrl + 'api/dir/create/', {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  method: 'POST',
-  body: JSON.stringify({name: data.value, id: null, parent_id: null}),
-})
-.then(checkStatus)
-.then(res => {
-  return "notebook created";
-}, e => {
-  return Promise.reject();
-});
+export {
+  getMenu,
+  getContent,
+  createNote,
+  createDir,
+  deleteDir,
+  deleteNote,
+  renameDir,
+  renameNote,
+  updateNote
+};
 
-// delete a existing notebook(directory)
-export const deleteDir = (id) => fetch(baseUrl + `api/dir/delete/${id}`, {
-  method: 'DELETE',
-})
-.then(checkStatus)
-.then(res => {
-  return "notebook deleted";
-}, e => {
-  return Promise.reject();
-});
-
-// delete a existing note
-export const deleteNote = (id) => fetch(baseUrl + `api/note/delete/${id}`, {
-  method: 'DELETE',
-})
-.then(checkStatus)
-.then(res => {
-  return "note deleted";
-}, e => {
-  return Promise.reject();
-});
-
-// rename a existing notebook(directory)
-export const renameDir = (id, data) => fetch(baseUrl + 'api/dir/update/', {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  method: 'PUT',
-  body: JSON.stringify({ name: data.value, id: id }),
-})
-.then(checkStatus)
-.then(res => {
-  return "notebook updated";
-}, e => {
-  return Promise.reject();
-});
-
-// rename a existing note
-export const renameNote = (id, data) => fetch(baseUrl + 'api/note/update/', {
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  method: 'PUT',
-  body: JSON.stringify({ title: data.value, id: id }),
-})
-.then(checkStatus)
-.then(res => {
-  return "note updated";
-}, e => {
-  return Promise.reject();
-});
