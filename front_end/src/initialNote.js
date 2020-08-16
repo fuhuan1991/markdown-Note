3507717225bb15c74435267da79c14c4f18dfb2c
fuhuan1991 <huanfu2019@gmail.com>
2020-08-16 11:24:56 -0400

    added initial note content

---
+++ front_end/src/initialNote.js
@@ -0,0 +1,56 @@
+const initialText = "## This is your new Markdown note\n" +
+"All the frequenly used Markdown syntax are supported. For more infor about the syntax, please check [here](https://www.markdownguide.org/basic-syntax/).\n" +
+"You can use insertion button on the top to insert Markdown code templetes or you can write whatever Markdown you like\n" +
+"<br><br>\n" +
+"\n" +
+"## Examples\n" +
+"---------------\n" +
+"#### Bold&Italic:\n" +
+"\n" +
+"**bold** text.\n" +
+"\n" +
+"__bold__ text.\n" +
+"\n" +
+"*Italicized* text.\n" +
+"\n" +
+"<br>\n" +
+"\n" +
+"#### Lists:\n" +
+"\n" +
+"1. item\n" +
+"2. item\n" +
+"3. item\n" +
+"\n" +
+"- item\n" +
+"- item\n" +
+"- item\n" +
+"\n" +
+"\n" +
+"- [x]  item\n" +
+"- [ ]  item\n" +
+"- [ ]  item\n" +
+"\n" +
+"<br>\n" +
+"\n" +
+"#### Tables\n" +
+"|       | col-1 | col-2 | col-3 | col-4 | col-5 |\n" +
+"|-     -|-     -|-     -|-     -|-     -|-     -|\n" +
+"| row-1 |       |       |       |       |       |\n" +
+"| row-2 |       |       |       |       |       |\n" +
+"| row-3 |       |       |       |       |       |\n" +
+"\n" +
+"<br>\n" +
+"\n" +
+"#### Links\n" +
+"This is a link: [link](https://en.wikipedia.org/wiki/Don_Quixote)\n" +
+"\n" +
+"This is also a link: https://en.wikipedia.org/wiki/Don_Quixote\n" +
+"\n" +
+"<br>\n" +
+"\n" +
+"#### Pictures\n" +
+"\n" +
+"![alt text](https://upload.wikimedia.org/wikipedia/commons/2/20/Don_Quijote_and_Sancho_Panza.jpg)\n"
+
+
+export default initialText;
