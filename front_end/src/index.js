5024de330ff17348bed3182f2f5f1678849fb284
fuhuan1991 <huanfu2019@gmail.com>
2020-08-17 11:06:47 -0400

    switch to hash router to solve refresh issue

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/index.jsx';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { basename } from './config';
 
 import './index.scss';
