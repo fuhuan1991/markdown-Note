095fe1b222da90c40aa6d274d738f603757bfd3f
fuhuan1991 <huanfu2019@gmail.com>
2020-08-22 16:47:59 -0400

    added font size button

--- front_end/src/mkNote/mkDisplay/index.jsx
+++ front_end/src/mkNote/mkDisplay/index.jsx
@@ -2,19 +2,47 @@ import React from 'react';
 import Markdown from 'react-markdown';
 import './style.scss';
 
-const MkDisplay = (props) => {
+class MkDisplay extends React.Component {
 
-  const { shrink, expand } = props;
-  let className = 'mk-display';
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontSize: 16, 
+    }
+  }
 
-  if (shrink) className += ' shrink';
-  if (expand) className += ' expand';
+  incFontSize = () => {
+    this.setState({ fontSize: this.state.fontSize + 1});
+  }
 
-  return (
-    <div className={className}>
-      <Markdown source={props.source} escapeHtml={false}/>
-    </div>
-  );
+  resetFontSize = () => {
+    this.setState({ fontSize: 16});
+  }
+
+  decFontSize = () => {
+    this.setState({ fontSize: this.state.fontSize - 1});
+  }
+
+  render () {
+    const { shrink, expand, source } = this.props;
+    const { fontSize } = this.state;
+    let className = 'mk-display';
+
+    if (shrink) className += ' shrink';
+    if (expand) className += ' expand';
+
+    return (
+      <div className={className} style={{fontSize: fontSize + 'px'}}>
+        <div className="font-size" >
+          Font size:
+          <span className='option' onClick={this.incFontSize}>+</span>
+          <span className='option' onClick={this.resetFontSize}>Reset</span>
+          <span className='option' onClick={this.decFontSize}>-</span>
+        </div>
+        <Markdown source={source} escapeHtml={false}/>
+      </div>
+    );
+  }
 }
 
 export default MkDisplay;
