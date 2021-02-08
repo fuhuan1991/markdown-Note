4f703dc1576233ea036bbc588411b1e5e060df9d
fuhuan1991 <huanfu2019@gmail.com>
2021-02-08 16:33:09 -0500

    build

---
+++ build/precache-manifest.360b68396fa7e9e0ad1a3b057094dea5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48304932a9eaaca89faded5bab66ff0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "896977b10594b9788257",
+    "url": "/static/css/2.6b59a612.chunk.css"
+  },
+  {
+    "revision": "b9d95b656ef2f72e866f",
+    "url": "/static/css/main.5bb4ed16.chunk.css"
+  },
+  {
+    "revision": "896977b10594b9788257",
+    "url": "/static/js/2.27166af5.chunk.js"
+  },
+  {
+    "revision": "0b1dab969522db757c480dac0bf2c36d",
+    "url": "/static/js/2.27166af5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d95b656ef2f72e866f",
+    "url": "/static/js/main.07a9f488.chunk.js"
+  },
+  {
+    "revision": "00068c85fecb8dfe32eb",
+    "url": "/static/js/runtime-main.a22c96bd.js"
+  }
+]);
